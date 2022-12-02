e0313453a5425246feb418eaaf8817c73a59b09c
Seymu1R <Seymuram@code.edu.az>
2022-12-03 02:00:38 +0400

    Addind some Changing

---
+++ ViewComponents/SlideStudentViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using EduHomeProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduHomeProject.ViewComponents
+{
+    public class SlideStudentViewComponent:ViewComponent
+    {
+        public async Task< IViewComponentResult> InvokeAsync()
+        {            
+            return  View();
+        }
+    }
+}
